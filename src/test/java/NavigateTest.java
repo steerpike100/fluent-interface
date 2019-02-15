cb68cc80b406724f78df7b14753e5d73f8733cbe
stephenb <stephen.bartholomew@paxport.net>
2019-02-15 09:36:22 +0000

    Blah

--- src/test/java/NavigateTest.java
+++ src/test/java/NavigateTest.java
@@ -1,5 +1,6 @@
 import org.testng.annotations.Test;
 import pluralsight.pages.*;
+import pluralsight.utils.DistanceFrom;
 
 import java.util.List;
 
@@ -9,6 +10,8 @@ import static pluralsight.pages.EnglandHomePage.getEnglandHomePage;
 import static pluralsight.pages.HomePage.getHomePage;
 import static pluralsight.pages.LocalNewsPage.*;
 import static pluralsight.pages.NewsPage.getNewsPage;
+import static pluralsight.utils.LocalNewsPageActionController.localNewsSavedDiv;
+import static pluralsight.utils.LocalNewsPageActionController.locationHeader;
 
 public class NavigateTest extends BaseTestClass {
 
@@ -19,30 +22,29 @@ public class NavigateTest extends BaseTestClass {
     AbstractPage abstractPage = getAbstractPage();
 
 
-//    @Test
-//    public void basicFilterByTest() throws InterruptedException {
-//
-//        home.act().clickNewsButton();
-//
-//        news.act().clickEnglandButton();
-//
-//        england.act().clickLocalNewsButton();
-//
-//        local.act().setAsLocalNews();
-//
-//        abstractPage.verify()
-//                .verifyIsDisplayed(localNewsSavedDiv());
-//
-//        local.act().setDistance(DistanceFrom.FIVEMILES);
-//
-//        abstractPage.verify()
-//                .verifyIsDisplayed(locationHeader());
-//
-//    }
+    @Test
+    public void basicFilterByTest() throws InterruptedException {
+
+        home.act().clickNewsButton();
+
+        news.act().clickEnglandButton();
+
+        england.act().clickLocalNewsButton();
+
+        local.act().setAsLocalNews();
+
+        abstractPage.verify()
+                .verifyIsDisplayed(localNewsSavedDiv());
+
+        local.act().setDistance(DistanceFrom.FIVEMILES);
+
+        abstractPage.verify()
+                .verifyIsDisplayed(locationHeader());
+
+    }
 
     @Test
     public void regionListcheck(){
-
         List<String> regionList =  news.get().categories();
         assertThat(regionList)
                 .hasSize(19)
